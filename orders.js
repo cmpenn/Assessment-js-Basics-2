a538263b6fd7f8ce5eb35acf69a534abebb9543e
Christian Penn <cpenn1147@gmail.com>
2022-10-07 12:19:31 -0600

    finished orders

--- orders.js
+++ orders.js
@@ -30,7 +30,18 @@
 */
 
 //CODE HERE
-
+class Ticket{
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';
+    }
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log(`the order for ${this.customerId} is now ${newStatus}`)
+    }
+}
 
 
 /*
@@ -45,7 +56,7 @@
 */
 
 //CODE HERE
-
+let firstTicket = new Ticket('pizza, bread, and soda', '7:03 pm', 575)
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +65,5 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus('cooking')
